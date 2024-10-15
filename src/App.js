c90bba0a12fea849c049b6fff873534b4db0ec96
Thomas-ARKADY <thomasdel@arkady.io>
2024-10-15 12:07:11 +0200

    full working game

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ChromeDino from 'react-chrome-dino';
+
+function App() {
+  return (
+    <div className="App">
+      <text>My Dino Game du beau gosse !!!! </text>
+
+      <ChromeDino />
+    </div>
+  );
+}
+
+export default App;
